892055966bf3bb82b9ac18df34e0bfb946ae64d6
Warrenym <navarrowarrenlloyd@gmail.com>
2026-05-13 13:00:43 +0800

    Optimized Relics Tab (Inventory)

--- Assets/Scripts/GameManager/Slot.cs
+++ Assets/Scripts/GameManager/Slot.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
     public GameObject currentItem;
-
+    public Image slotActivate;
     public void SetItem(GameObject item)
     {
         currentItem = item;
